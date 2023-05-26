36aa3193ffb5115b86b22bae338aa39c31232d90
Pavel <ppovolyaev@inbox.ru>
2023-05-26 03:57:57 +0300

    динамический степпер

--- src/app/components/workspace/create-order/create-order.component.ts
+++ src/app/components/workspace/create-order/create-order.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
 import {MatStepper} from "@angular/material/stepper";
+import {Time} from "@angular/common";
 
 
 @Component({
@@ -10,65 +11,81 @@ import {MatStepper} from "@angular/material/stepper";
 })
 export class CreateOrderComponent {
 
-
-  isEditable = false;
-
   constructor(private _formBuilder: FormBuilder) {}
 
   selectDataVisible: boolean;
   text!: string;
-  selectedDate!: Date;
 
+  singleStepperV: boolean=false;
   emailStepperV: boolean=false;
   smsStepperV: boolean=false;
   StartVisible:boolean=true;
   btnExVisible:boolean=false;
 
+  emailChoose: boolean=false;
+  smsChoose:boolean=false;
 
-  startChoose: any;
-  chooseTime: any;
-  private CdkStepperr: any;
+  selectedDate!: Date;
+  chooseTime: Time;
 
 
   onChooseDate(event: Event) {
       this.selectDataVisible = true;
     }
 
-
   onStartImme(event: Event)
   {
       this.selectDataVisible = false;
+  }
+
+  CheckSms() {
+    if(!this.smsChoose)
+    {
+      this.smsChoose=true;
     }
+    else {
+      this.smsChoose = true;
+    }
+  }
 
-
+  CheckEmail() {
+    if(!this.emailChoose) {
+      this.emailChoose = true;
+    }
+    else{
+      this.emailChoose=false;
+    }
+  }
 
   start()
   {
-    if(this.startChoose=="sms")
-    {
-      this.StartVisible=false;
+    this.singleStepperV=true;
+    if(this.smsChoose) {
+      this.emailStepperV = false;
       this.smsStepperV = true;
-      this.btnExVisible=true;
-
     }
-      if(this.startChoose=="email")
-      {
-        this.StartVisible=false;
+    if(this.emailChoose) {
         this.emailStepperV = true;
-        this.btnExVisible=true;
-      }
+        this.smsStepperV = false;
+    }
+    if(this.emailChoose && this.smsChoose) {
+        this.emailStepperV = true;
+        this.smsStepperV = true;
+    }
+    this.StartVisible=false;
+    this.btnExVisible=true;
   }
 
-  @ViewChild('emailStepper') email!: MatStepper;
-  @ViewChild('smsStepper') sms!: MatStepper;
-  restart(stepper1: MatStepper,stepper2: MatStepper)
+  @ViewChild('singleStepper') s!: MatStepper;
+  restart(stepper: MatStepper)
   {
-    stepper1.reset()
-    stepper2.reset()
+    stepper.reset()
     this.StartVisible=true;
     this.smsStepperV = false;
     this.btnExVisible=false;
     this.emailStepperV = false;
+    this.singleStepperV=false;
   }
+
 
 }
