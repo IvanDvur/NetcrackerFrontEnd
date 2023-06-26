bbd3edb408db0a5b3cb141e124b7beecac063c08
ivand <ivandvurechenskiy26@gmail.com>
2023-06-26 16:38:33 +0300

    billing service

---
+++ src/app/services/billing/billing.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TokenDto} from "../auth/token-dto";
+
+
+const changeTariffURL = 'http://localhost:8080/tariff/change'
+@Injectable({
+  providedIn: 'root'
+})
+export class BillingService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  changeTariff(tariff: string):Observable<TokenDto>{
+    const formData:FormData = new FormData()
+    formData.append('plan',tariff)
+    return this.httpClient.post<TokenDto>(changeTariffURL,formData)
+  }
+
+}
